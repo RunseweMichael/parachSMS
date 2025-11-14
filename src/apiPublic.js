58cbe401cfbfc3b8cb33a2bf3f73511d9601909d
KingSeraph07 <runsewemichael93@gmail.com>
2025-11-14 11:13:30 +0100

    deployed backend and added correct url to components

--- src/apiPublic.js
+++ src/apiPublic.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const apiPublic = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  // baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: 'https://parachsms.pythonanywhere.com/api/',
 });
 
 export default apiPublic;
