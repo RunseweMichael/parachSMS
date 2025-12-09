9fea4a5173a90eb8a2ed6e2552288791649122f5
KingSeraph07 <runsewemichael93@gmail.com>
2025-12-09 05:48:37 +0100

    final fix

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     
     // tailwind-animate is a Tailwind plugin → put it in tailwind.config.js
   ],
-  base: process.env.VITE_BASE_PATH || '/parachSMS',
+  base: process.env.VITE_BASE_PATH || '/',
 
   
 });
