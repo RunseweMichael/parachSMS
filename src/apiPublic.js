e63e60629ccaae7fa69fa8a080e0ad862d02fb7d
KingSeraph07 <runsewemichael93@gmail.com>
2026-09-19 17:54:53 +0100

    I added/updated the admin payment discount. so the admin can apply discounts directly to a student payment info

--- src/apiPublic.js
+++ src/apiPublic.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiPublic = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
-    // baseURL: 'https://studentmgt.whalesharkengineering.com.ng/api/',
+  // baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: 'https://studentmgt.whalesharkengineering.com.ng/api/',
 });
 
 
